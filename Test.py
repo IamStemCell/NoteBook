d3c0bf1c264167fa7983c68db1dd4a44d29a87ac
IamStemCell <113768157+IamStemCell@users.noreply.github.com>
2023-11-22 16:55:58 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,8 @@
-create repo 
-create file
-open file in dev space
-create nb
+create repo on github
+create file in that repo
+open file in dev space open github dev
+file-> create new nb 
 commit+push on main branch into repo
-open in G
+open in G 
 save copy to clb g drive colab notebooks 
+g drive colab notebooks directory has that nbk
